54faa4c43946997224e55426bacec26271c6fc38
Ashish-Kumar-Dash <ashishdash2410@gmail.com>
2024-10-30 19:14:28 +0530

    cp.c

--- cp.c
+++ cp.c
@@ -1,11 +1,14 @@
 //https://codeforces.com/problemset/problem/2020/A#:~:text=Problem%20%2D%202020A%20%2D%20Codeforces&text=You%20are%20given%20two%20integers,any%20non%2Dnegative%20integer%20x.
 #include <stdio.h>
 #include <math.h>
-int main() {
+int main() { int t; scanf("%d",&t);
+while(t--){
     int n,k; scanf("%d %d",&n,&k);
-if(k==1){printf("%d\n",n); }
-if(n<k){printf("%d",n); }
-if(n%k==0){printf("1\n"); }
+if(k==1){printf("%d\n",n); continue; }
+if(n<k){printf("%d\n",n); continue;} int y=0;
+for(long int i=0;i<100;i++){
+if(n-pow(k,i)==0){y++; break;}}
+if(y!=0){printf("1\n"); continue;}
 int i; 
 for(i=0;i<30;i++){
   if(n-pow(k,i)<0){break;}
@@ -20,6 +23,4 @@ for(int j=i-1;j>=0;j--){
     }
 }
 printf("%d\n",c);
-
-    return 0;
 }
